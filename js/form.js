f10ee00a2e6260cab61ec90ff9ddecbacb6fc0f2
Rob <barrrobert84@gmail.com>
2023-08-27 18:22:11 +0200

    Implememnt dark mode

--- js/form.js
+++ js/form.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('#questionForm')
-const questionContainer = document.querySelector('.question--card')
+const questionCardContainer = document.querySelector('.question--card--container')
 const questionInput = document.querySelector('#question');
 const answerInput = document.querySelector('#answer');
 const characterCount1 = document.querySelector('.current1');
 const characterCount2 = document.querySelector('.current2');
 const maxLength = 150;
+
 
 questionInput.addEventListener('input', (e) => {
     const currentLength = questionInput.value.length
@@ -21,6 +22,11 @@ answerInput.addEventListener('input', (e) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    const formPageQuestionCard = document.createElement('article')
+    formPageQuestionCard.classList.add('card--question')
+    questionCardContainer.append(formPageQuestionCard)
+
+
     const questionText = e.target.elements.question.value;
     const answerText = e.target.elements.answer.value;
     const tagText = e.target.elements.tag.value;
@@ -28,21 +34,26 @@ form.addEventListener('submit', (e) => {
     const bookmark = document.createElement('img')
     bookmark.src = './images/bookmark.png'
     bookmark.classList.add('card--bookmark')
-    questionContainer.append(bookmark)
+    formPageQuestionCard.append(bookmark)
+
+    bookmark.addEventListener('click', (e) => {
+        bookmark.classList.toggle('bookmark--click')
+    })
+    
 
     const questionCardHeader = document.createElement('h2');
     questionCardHeader.innerText = questionText;
-    questionContainer.append(questionCardHeader);
+    formPageQuestionCard.append(questionCardHeader);
 
     const showAnswer = document.createElement('button')
     showAnswer.classList.add('show--answer')
     showAnswer.innerHTML = 'Show Answer'
-    questionContainer.append(showAnswer)
+    formPageQuestionCard.append(showAnswer)
 
     const questionCardAnswer = document.createElement('h2');
     questionCardAnswer.classList.add('card--answer')
     questionCardAnswer.innerText = answerText;
-    questionContainer.append(questionCardAnswer);
+    formPageQuestionCard.append(questionCardAnswer);
 
     showAnswer.addEventListener('click', (e) => {
        questionCardAnswer.classList.toggle('hidden')
@@ -59,9 +70,11 @@ form.addEventListener('submit', (e) => {
     const questionCardTag = document.createElement('p');
     questionCardTag.classList.add('form-hashtag-container')
     questionCardTag.innerText = `#${tagText}`;
-    questionContainer.append(questionCardTag)
+    formPageQuestionCard.append(questionCardTag)
 
-    
+    e.target.reset()
+    characterCount1.textContent = '0';  
+    characterCount2.textContent = '0'; 
 
     
     
